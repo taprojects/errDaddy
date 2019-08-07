56b68e4827a9d29f710856b055e0a9f00d3528ed
UncleJerry <jared.myhrberg@gmail.com>
2019-08-07 16:28:17 -0700

    its fucked

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,23 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import NavBar from '../containers/NavBar';
 import NewErr from '../containers/NewErr';
 import ErrContainer from '../containers/ErrContainer';
 import DisplayError from '../containers/DisplayError';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 export default function App() {
   return (
-    <Router>
+    <Router >
+      <NavBar history={history}/>
       <Switch>
         <Route path="/search/:searchTerm" component={ErrContainer} />
         <Route path="/newErr" component={NewErr} />
         <Route path="/displayErr/:errId" component={DisplayError} />
-        <Route path="/displayErr" component={DisplayError} />
       </Switch>
-
     </Router>
   );
 }
--- src/containers/NewErr.js
+++ src/containers/NewErr.js
@@ -1,45 +1,27 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import NavBar from '../components/nav/NavBar';
-import { setSearchTerm } from '../actions/setSearchTerm';
-import { createNewError, getAllTags } from '../actions/errorActions';
+import { createNewError } from '../actions/errorActions';
 import ErrorForm from '../components/errForm/ErrorForm';
-import { selectTags } from '../selectors/errSelectors';
 import { FormStyle } from '../styles/form.style';
 
 
 class NewErr extends PureComponent {
   static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
     onFormSubmit: PropTypes.func.isRequired,
-    history: PropTypes.object.isRequired,
-    fetchTags: PropTypes.func.isRequired,
-    tags: PropTypes.array
+    history: PropTypes.object.isRequired
   };
 
   state = {
-    searchTerm: '',
     title: '',
     errCode: '',
     description: '',
     solution: '',
     tags: ''
   };
-
-  componentDidMount() {
-    if(!this.props.tags) this.props.fetchTags();
-  } 
   
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
-  }
-  
-  handleSearchSubmit = event => {
-    event.preventDefault();
-    const { searchTerm } = this.state;
-    this.props.onSubmit(searchTerm);
-    this.props.history.push(`/search/${searchTerm}`);
   }
   
   handleFormSubmit = event => {
@@ -54,50 +36,24 @@ class NewErr extends PureComponent {
     this.props.history.push('/displayErr');
   }
 
-  handleRecent = () => {
-    this.props.onSubmit('recent');
-    this.props.history.push('/search/recent');
-  }
-
   render() {
-    if(this.props.tags) {
-      return (
-        <>
-        <NavBar 
-          handleChange={this.handleChange} 
-          handleSubmit={this.handleFormSubmit} 
-          tagArr={this.props.tags} 
-          handleRecent={this.handleRecent} 
-        />
-        <FormStyle>
-          <h1>New Solved Err Babbbieeee</h1>
-          <ErrorForm handleChange={this.handleChange} handleFormSubmit={this.handleFormSubmit}/>
-        </FormStyle>
-      </>
-      );
-    } else {
-      return <h1>Loading</h1>;
-    }
+
+    return (
+      <FormStyle>
+        <h1>New Solved Err Babbbieeee</h1>
+        <ErrorForm handleChange={this.handleChange} handleFormSubmit={this.handleFormSubmit}/>
+      </FormStyle>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  tags: selectTags(state)
-});
-
 const mapDispatchToProps = (dispatch) => ({
-  onSubmit(searchTerm) {
-    dispatch(setSearchTerm(searchTerm));
-  },
   onFormSubmit({ title, description, solution, tags }) {
     dispatch(createNewError({ title, description, solution, tags }));
-  },
-  fetchTags() {
-    dispatch(getAllTags());
   }
 });
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(NewErr);
